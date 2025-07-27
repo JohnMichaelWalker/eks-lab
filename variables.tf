3454d267d3b0a3df4a3baaf67e4d487bfc772677
John Walker <john_walker_333@hotmail.com>
2025-07-27 17:01:29 +0100

    Add external dns role

--- variables.tf
+++ variables.tf
@@ -9,3 +9,15 @@ variable "cluster_name" {
   type = string
   default = "eks-lab-cluster"
 }
+
+variable "external_dns_namespace" {
+  description = "Namespace that the external dns is in"
+  type = string
+  default = "kube-system"
+}
+
+variable "external_dns_service_account_name" {
+  description = "Service account name for the external dns"
+  type = string
+  default = "external-dns"
+}
